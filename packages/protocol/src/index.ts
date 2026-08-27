d57ceb2139d6d6dd6bf3aea40b9e43001f72d035
ErrorDoc404 <akashsinhakvs@gmail.com>
2026-08-27 20:43:52 +0530

    build(protocol): add package entry point

---
+++ packages/protocol/src/index.ts
@@ -0,0 +1,4 @@
+/** Public entry point for versioned Lavalink protocol contracts. */
+export * from "./version.js";
+export { V4_PROTOCOL } from "./v4/index.js";
+export { V5_PROTOCOL } from "./v5/index.js";
